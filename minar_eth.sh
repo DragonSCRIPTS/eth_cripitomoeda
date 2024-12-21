61b28a1a190e1bfb5248e665bbd66df4273b29fc
DragonSCRIPTS <cleyltondragonscp@gmail.com>
2024-12-21 18:08:56 -0300

    Create minar_eth.sh

---
+++ minar_eth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Script para minerar Ethereum com ethminer
+
+# Substitua pelo seu endereço de carteira Ethereum
+ethminer -P stratum1+tcp://0x8b38661e6A3d3A278529B84c1Fe0157F67379728@eth.ethermine.org:4444
